6ae897520d3f177163030ec72cd9faaec959f8b7
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2024-10-21 03:10:47 +0300

    feat: task done!

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.val = 0;
+    this.min = 0;
+    this.max = 0;
+    }
 
     setRange(min, max) {
-
+    this.min = min;
+    this.max = max;
     }
 
     guess() {
-
+        this.val = Math.ceil((this.max + this.min) /2);
+        return this.val;
     }
 
     lower() {
-
+        this.max = this.val;
     }
 
     greater() {
-
+        this.min = this.val;
     }
 }
 
